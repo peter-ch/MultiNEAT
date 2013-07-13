3b1a6c404b180ba1db50a7278e6dae684b08f937
Peter Chervenski <spookey@abv.bg>
2013-07-13 19:11:36 +0300

    Fixed makefiles.

--- Release/subdir.mk
+++ Release/subdir.mk
@@ -48,10 +48,10 @@ CPP_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 %.o: ../%.cpp
-    @echo 'Building file: $<'
-    @echo 'Invoking: GCC C++ Compiler'
-    g++ -I/usr/include/python2.7 -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
-    @echo 'Finished building: $<'
-    @echo ' '
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/usr/include/python2.7 -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
 
 
