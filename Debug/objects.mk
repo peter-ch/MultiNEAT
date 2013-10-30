d361660aebd2c421540a17fac01a94589cc8cd06
Peter Chervenski <spookey@abv.bg>
2013-10-30 11:33:39 +0200

    Added Debug/Release directories generated by Eclipse.

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lboost_python -lboost_serialization -lpython2.7
+
---
+++ Release/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lpython2.7 -lboost_serialization -lboost_python
+
