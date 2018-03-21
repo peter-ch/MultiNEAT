aa0152de64b96c930978d5ff97b61b534721df2b
Anton Matosov <anton.matosov@gmail.com>
2018-03-21 00:03:33 -0700

    Add `yes` to overcome stucking https://github.com/Anaconda-Platform/anaconda-client/issues/282

--- travis/run.sh
+++ travis/run.sh
@@ -5,7 +5,7 @@ set -x
 source $HOME/miniconda/etc/profile.d/conda.sh
 conda activate test-environment
 
-anaconda login --username $CONDA_LOGIN_USERNAME --password $CONDA_LOGIN_PASSWORD
+yes | anaconda login --username $CONDA_LOGIN_USERNAME --password $CONDA_LOGIN_PASSWORD
 conda config --set anaconda_upload ${CONDA_UPLOAD:-no}
 
 conda build conda/
