76a6c001018c96766bf23184f95eed268f3001b5
Anton Matosov <anton.matosov@gmail.com>
2018-03-23 01:58:52 -0700

    Implement parallel builds hack

--- setup.py
+++ setup.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python
+
 from __future__ import print_function
 from setuptools import setup, Extension
 import sys
 import os
+import psutil
+
+# monkey-patch for parallel compilation
+def parallelCCompile(self, sources, output_dir=None, macros=None, include_dirs=None, debug=0, extra_preargs=None, extra_postargs=None, depends=None):
+    # those lines are copied from distutils.ccompiler.CCompiler directly
+    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(output_dir, macros, include_dirs, sources, depends, extra_postargs)
+    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
+    # parallel code
+    
+    N = psutil.cpu_count(logical=False) # number of parallel compilations
+    import multiprocessing.pool
+    def _single_compile(obj):
+        try: src, ext = build[obj]
+        except KeyError: return
+        self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+    # convert to list, imap is evaluated on-demand
+    list(multiprocessing.pool.ThreadPool(N).imap(_single_compile,objects))
+    return objects
+
+import distutils.ccompiler
+distutils.ccompiler.CCompiler.compile=parallelCCompile
+
 
 ''' Note:
 
