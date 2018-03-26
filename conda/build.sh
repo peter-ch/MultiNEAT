9612019b79bff2e436f5160048b74d2dcd91d454
Anton Matosov <anton.matosov@gmail.com>
2018-03-25 23:40:19 -0700

    Remove ccache entirely as it fails build on windows

--- conda/build.sh
+++ conda/build.sh
@@ -5,12 +5,6 @@ set -e
 
 export MN_BUILD=boost
 
-
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-  export CC="ccache gcc"
-  export CXX="ccache g++"
-fi
-
 SOURCE_CODE_DIR=${SRC_DIR:-$(dirname $0)/..}
 
 python ${SOURCE_CODE_DIR}/setup.py build_ext
