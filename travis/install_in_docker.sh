276a58b74a79a2ec15c10d07d302f46287ed2ce5
Anton Matosov <anton.matosov@gmail.com>
2018-03-26 16:42:42 -0700

    Try docker on travis

---
+++ travis/install_in_docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+conda config --set always_yes yes --set changeps1 no
+
+conda update -q conda
+
+conda info -a
+
+conda create -q -n test-environment python=3.6 anaconda-client conda-build
+conda activate test-environment
