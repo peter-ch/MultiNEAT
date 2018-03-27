85853264bcb540e2dd1d2fd114e4c2d2706a4d58
Anton Matosov <anton.matosov@gmail.com>
2018-03-26 22:28:48 -0700

    Add prefix/bin to path as otherwise python can't be found at all

--- conda/run_test.bat
+++ conda/run_test.bat
@@ -1,7 +1,8 @@
 
+echo "PATH = %PATH%"
 
 REM Make sure to use proper python from conda
-set PATH=C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\
+set PATH=%PREFIX%\bin:C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\
 
 python "%SRC_DIR%/examples/TestTraits.py"
 REM python "%SRC_DIR%/examples/NoveltySearch.py"
