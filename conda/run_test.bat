4c543d13619046d1c6bd1165356bff3c48ae9328
Anton Matosov <anton.matosov@gmail.com>
2018-03-27 00:28:13 -0700

    Try using correct PATH on windows.
    Right now generated package works correctly, just conda tests are messed
    up on Appsveyor

--- conda/run_test.bat
+++ conda/run_test.bat
@@ -1,10 +1,10 @@
 
-REM echo "PATH = %PATH%"
+echo "PATH = %PATH%"
 
-REM REM Make sure to use proper python from conda
-REM set PATH=%PREFIX%\bin:C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\
+REM Make sure to use proper python from conda
+set PATH=%PREFIX%:C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\
 
-REM python "%SRC_DIR%/examples/TestTraits.py"
-REM REM python "%SRC_DIR%/examples/NoveltySearch.py"
-REM python "%SRC_DIR%/examples/TestNEAT_xor.py"
-REM python "%SRC_DIR%/examples/TestHyperNEAT_xor.py"
+python "%SRC_DIR%/examples/TestTraits.py"
+REM python "%SRC_DIR%/examples/NoveltySearch.py"
+python "%SRC_DIR%/examples/TestNEAT_xor.py"
+python "%SRC_DIR%/examples/TestHyperNEAT_xor.py"
