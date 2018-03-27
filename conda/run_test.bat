2eaa63db1206ef4914c82ebfc4e1f40da15c414d
Anton Matosov <anton.matosov@gmail.com>
2018-03-27 09:30:23 -0700

    Do not reset PATH as dll loading of extention fails

--- conda/run_test.bat
+++ conda/run_test.bat
@@ -2,9 +2,6 @@
 REM show test env before latering it
 set
 
-REM Make sure to use proper python from conda
-set PATH=%PREFIX%:C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\
-
 %PYTHON% "%SRC_DIR%/examples/TestTraits.py"
 REM %PYTHON% "%SRC_DIR%/examples/NoveltySearch.py"
 %PYTHON% "%SRC_DIR%/examples/TestNEAT_xor.py"
