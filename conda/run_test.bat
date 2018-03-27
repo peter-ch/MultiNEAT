749befa01da18d075fb791d146ccf1f67e3a72d2
Anton Matosov <anton.matosov@gmail.com>
2018-03-27 01:35:19 -0700

    Use the env from conda-build

--- conda/run_test.bat
+++ conda/run_test.bat
@@ -5,7 +5,7 @@ set
 REM Make sure to use proper python from conda
 set PATH=%PREFIX%:C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\
 
-%CONDA_PYTHON_EXE% "%SRC_DIR%/examples/TestTraits.py"
-REM %CONDA_PYTHON_EXE% "%SRC_DIR%/examples/NoveltySearch.py"
-%CONDA_PYTHON_EXE% "%SRC_DIR%/examples/TestNEAT_xor.py"
-%CONDA_PYTHON_EXE% "%SRC_DIR%/examples/TestHyperNEAT_xor.py"
+%PYTHON% "%SRC_DIR%/examples/TestTraits.py"
+REM %PYTHON% "%SRC_DIR%/examples/NoveltySearch.py"
+%PYTHON% "%SRC_DIR%/examples/TestNEAT_xor.py"
+%PYTHON% "%SRC_DIR%/examples/TestHyperNEAT_xor.py"
