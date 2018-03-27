5354c2e671b6bdc220c096b9fcd96aa8f259ab56
Anton Matosov <anton.matosov@gmail.com>
2018-03-26 22:11:22 -0700

    Pass env vars

--- travis/run_in_docker.sh
+++ travis/run_in_docker.sh
@@ -2,4 +2,8 @@
 set -e
 set -x
 
-docker run -it --rm -v $PWD:/neat multineat:latest /bin/bash -c "/neat/travis/run.sh"
+docker run -it --rm -v $PWD:/neat multineat:latest \
+  -e CONDA_UPLOAD \
+  -e CONDA_LOGIN_USERNAME \
+  -e CONDA_LOGIN_PASSWORD  \
+  /bin/bash -c "/neat/travis/run.sh"
