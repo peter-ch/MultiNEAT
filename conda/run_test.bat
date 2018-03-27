81845fec62b24068865bb10b0f3587bb10fbb7e2
Anton Matosov <anton.matosov@gmail.com>
2018-03-27 01:32:57 -0700

    Try using full path to python

--- conda/run_test.bat
+++ conda/run_test.bat
@@ -1,10 +1,11 @@
 
-echo "PATH = %PATH%"
+REM show test env before latering it
+set
 
 REM Make sure to use proper python from conda
 set PATH=%PREFIX%:C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\
 
-python "%SRC_DIR%/examples/TestTraits.py"
-REM python "%SRC_DIR%/examples/NoveltySearch.py"
-python "%SRC_DIR%/examples/TestNEAT_xor.py"
-python "%SRC_DIR%/examples/TestHyperNEAT_xor.py"
+%CONDA_PYTHON_EXE% "%SRC_DIR%/examples/TestTraits.py"
+REM %CONDA_PYTHON_EXE% "%SRC_DIR%/examples/NoveltySearch.py"
+%CONDA_PYTHON_EXE% "%SRC_DIR%/examples/TestNEAT_xor.py"
+%CONDA_PYTHON_EXE% "%SRC_DIR%/examples/TestHyperNEAT_xor.py"
