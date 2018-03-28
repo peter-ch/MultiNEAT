52b80ae2a3b033e1c4c90eccacd9c12cc52917fe
Anton Matosov <anton.matosov@gmail.com>
2018-03-28 12:28:24 -0700

    Unify the run script

--- travis/linux.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-
-./travis/build_docker.sh
-./travis/run_in_docker.sh
--- travis/macos.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-
-./travis/install.sh
-./travis/run.sh
