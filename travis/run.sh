cda40072195bd9ba8c2ff2f69b5c50b456ec1169
Anton Matosov <anton.matosov@gmail.com>
2018-03-28 15:09:17 -0700

    Add conda_py to env for auth as otherwise login session is getting lost for parallel builds on travis

--- travis/run.sh
+++ travis/run.sh
@@ -10,7 +10,7 @@ cd "$script_dir/.."
 source $HOME/miniconda/etc/profile.d/conda.sh
 conda activate test-environment
 
-yes | anaconda login --username $CONDA_LOGIN_USERNAME --password $CONDA_LOGIN_PASSWORD --hostname MultiNEAT-TRAVIS-${TRAVIS_OS_NAME}-BUILD-${TRAVIS_BUILD_NUMBER}
+yes | anaconda login --username $CONDA_LOGIN_USERNAME --password $CONDA_LOGIN_PASSWORD --hostname MultiNEAT-TRAVIS-${TRAVIS_OS_NAME}-BUILD-${TRAVIS_BUILD_NUMBER}-${CONDA_PY}
 conda config --set anaconda_upload ${CONDA_UPLOAD:-no}
 
 conda build conda/ -c defaults -c conda-forge
